697a7fbf6ecb7673362506bf68b95bcf49621394
simpson101 <paul.simpson101@gmail.com>
2019-03-17 10:00:55 +0000

    update to rand num

--- index.js
+++ index.js
@@ -7,38 +7,51 @@ app.set('port',(process.env.PORT || 3000));
 app.use(express.static('public'));
 
 
-app.get("/GetImg", function (request, response) {
+app.get("/GetRand", function (request, response) {
 
-    var dream = request.query.dreamText;
+    
     var userId = request.query.userId;
+    var gobalRandom;
+    
 
-  //***********  TESTING NEW MC PUBLIC API    ******************
-   // then post to ZApier webhook with user id and prize 
-    var req = unirest("POST", "https://api.manychat.com/fb/sending/sendContent");
+  //***********  TESTING Random.org API    ******************
+   // 
+    unirest("POST", "https://www.random.org/strings/?num=1&len=10&digits=on&upperalpha=on&loweralpha=on&unique=on&format=plain&rnd=new")
+    
 
-    req.headers({
-        "Accept": "application/json",
-        "Authorization": "Bearer 233331847538713:a9735489b8b30abfe8424f2a66254abd",
-        "Content-Type": "application/json"
+    .end(function (response) {
+        gobalRandom = response.body;        
+        passValue()
+        response.json()
+      
       });
+  
+  function passValue(){
+    var y = gobalRandom;
+    console.log(y)
+    
+    response.json(
+      
+      {
+    
+   "version": "v2",
+    "content": {
+        
+        "actions": 
+            {
+                "action": "set_field_value",
+				"field_name": "S01- Offer Code",
+				"value":gobalRandom
+            }      
+    } 
+    
+  })
+    
+  }
 
-    req.form({
-			"subscriber_id": userId, 
-				"data": {"version": "v2", 
-					"content": { 
-					"messages": [ 
-									{ "type": "image", 
-									  "url": "https://res.cloudinary.com/trafficlighthouse/image/upload/l_text:courier_25_bold_underline:"+ request.query.code +",g_north,x_-65,y_110/l_text:courier_25_bold:"+ request.query.first +"%20"+ request.query.last +",g_north,x_-65,y_80/v1542797808/voucher2.png"
-                  } 
-								] 
-								} 
-
-						}
-			});
-
-    req.end(function (res) {
-        if (res.error) throw new Error(res.error);
-      });
+   
+ 
+  
   
 });
 
